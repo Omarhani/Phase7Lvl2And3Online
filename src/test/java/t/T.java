a3b422201f6233e69fc001e0eb90c31e6b32a10a
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2024-09-07 00:46:14 +0300

    add test case

---
+++ src/test/java/t/T.java
@@ -0,0 +1,13 @@
+package t;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+public class T extends BaseTests {
+
+    @Test
+    public void t1(){
+        System.out.println("Automation test");
+        System.out.println("With gitHub Actions");
+    }
+}
